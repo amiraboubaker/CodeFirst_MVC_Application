85f33762174397c033acb5559b3f9affcf88b28d
Amira Boubaker <amiraboubakeresprims@gmail.com>
2024-12-25 13:14:14 +0100

    add Edit Task

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -28,10 +28,26 @@ namespace MyApp.Controllers
             {
                 _context.Items.Add(item);
                 await _context.SaveChangesAsync();
-                return RedirectToAction("index");
+                return RedirectToAction("Index");
             }
             return View(item);
         }
-
+        
+        public async Task<IActionResult> Edit(int id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(x => x.Id == id);
+            return View(item);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id", "Name","Price")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
     }
 }
